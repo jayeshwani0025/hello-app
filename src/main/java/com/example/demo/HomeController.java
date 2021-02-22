c35ef5f8bc9c71bc73aa62690c1f74258314343e
JayeshWani <jayeshwani25@gmail.com>
2021-02-23 05:14:19 +0530

    changes

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/home")
+public class HomeController {
+
+	@GetMapping("/test")
+	public String getString() {
+		return "Hello everyone";
+	}
+}
